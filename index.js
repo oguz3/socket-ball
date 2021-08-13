b46a2fa51c51589144dc988e681039daba0a6a6b
oguz3 <ibrahimoguzhan.ulukaya@ogr.deu.edu.tr>
2021-08-13 20:15:52 +0300

    fix: add canvas for optimization

--- index.js
+++ index.js
@@ -12,24 +12,48 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
 	socket.on("adduser", (username) => {
 		socket.username = username;
-		usernames[username] = {id: socket.id, name: username, left: 0, top: 0};
+		usernames[username] = {
+			id: socket.id,
+			name: username,
+			oldLeft: 0,
+			oldTop: 0,
+			left: 0,
+			top: 0,
+		};
 		io.emit("adduser", username);
 	});
 
-	socket.on("hero_move", (username, x ,y) => {
+	socket.on("gameLoop", () => {
+		let newGame = {
+			width: 600,
+			height: 400,
+			heroes: Object.values(usernames),
+		};
+		io.emit("gameLoop", newGame);
+	});
+
+	socket.on("hero_move", (username, x, y) => {
 		let userNewPos = {
 			...usernames[username],
+			oldLeft:
+				usernames[username] && usernames[username].left
+					? usernames[username].left
+					: 0,
+			oldTop:
+				usernames[username] && usernames[username].top
+					? usernames[username].top
+					: 0,
 			top: y,
-			left: x
-		}
+			left: x,
+		};
 		usernames[username] = userNewPos;
 		io.emit("hero_move", userNewPos);
 	});
 
-	socket.on('disconnect', function() {
+	socket.on("disconnect", function () {
 		Object.values(usernames).map((user) => {
-			user.id === socket.id && delete usernames[user.name]
-		})
+			user.id === socket.id && delete usernames[user.name];
+		});
 	});
 });
 
